eec892e085f481f364c1df161b5781a22a7039da
klem231188 <chipie23>
2016-12-22 17:04:22 +0100

    2. The Hero Editor

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,25 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+<h1>{{title}}</h1>
+<h2>{{hero.name}} details!</h2>
+<div><label>id: </label>{{hero.id}}</div>
+<div>
+<label>name: </label>{{hero.name}}
+<input [(ngModel)]="hero.name" placeholder="name">
+</div>
+`,
 })
 export class AppComponent {
-  name = 'Angular';
+  title = 'Tour of Heores';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+
+export class Hero {
+  id: number;
+  name: string;
 }
