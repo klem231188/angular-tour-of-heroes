82304b5effd75b0588a8c20b53dc8de601f00197
klem231188 <clemtreguer@gmail.com>
2016-12-23 23:00:00 +0100

    6.Routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,22 +1,44 @@
-import {NgModule}      from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-
-import {AppComponent}  from './app.component';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
+import {AppComponent} from "./app.component";
+import {HeroesComponent} from "./heroes.component";
 import {HeroDetailComponent} from "./hero-detail.component";
+import {HeroService} from "./hero.service";
+import {RouterModule} from "@angular/router";
+import {DashboardComponent} from "./dashboard.component";
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
+    HeroesComponent,
     HeroDetailComponent
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  providers: [
+    HeroService
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
+
