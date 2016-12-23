e919a8ebec71b21ad47cdec5d978cda045018da8
klem231188 <chipie23>
2016-12-23 13:45:31 +0100

    4. Multiple Components

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent}  from './app.component';
 import {FormsModule} from "@angular/forms";
+import {HeroDetailComponent} from "./hero-detail.component";
 
 @NgModule({
   imports: [
@@ -10,7 +11,8 @@ import {FormsModule} from "@angular/forms";
     FormsModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   bootstrap: [
     AppComponent
